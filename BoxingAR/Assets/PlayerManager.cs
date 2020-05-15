5bef0510096749076e5840913e19eb6dc64fe9d9
Yessica Servin Dominguez <36170847+YessicaSD@users.noreply.github.com>
2020-05-15 10:12:06 +0200

    adding right and left hit

---
+++ BoxingAR/Assets/PlayerManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public Animator anim;
+   public enum PLAYER_STATES   
+    {
+        IDLE, 
+        RIGHT_HIT,
+        LEFT_HIT,
+    };
+
+   public PLAYER_STATES player_state = PLAYER_STATES.IDLE;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Debug.Log(player_state);
+       
+    }
+    public void RightPunch()
+    {
+        player_state = PLAYER_STATES.RIGHT_HIT;
+        anim.SetTrigger("RightHit");
+    }
+
+    public void LeftPunch()
+    {
+        player_state = PLAYER_STATES.LEFT_HIT;
+        anim.SetTrigger("LeftHit");
+    }
+}
