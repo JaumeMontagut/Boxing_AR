87e718a7eea7686f61eadc3fe834d6e1232f653a
JaumeMontagut <jaume.montagut.i.guix@gmail.com>
2020-05-16 16:58:35 +0200

    adding button icons

--- BoxingAR/Assets/Scripts/PlayerManager.cs
+++ BoxingAR/Assets/Scripts/PlayerManager.cs
@@ -4,37 +4,94 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-   private Animator anim;
-
    public enum PLAYER_STATES   
     {
         IDLE, 
         RIGHT_HIT,
         LEFT_HIT,
     };
+    
+    [HideInInspector] public PLAYER_STATES player_state = PLAYER_STATES.IDLE;
+    private Animator anim;
 
-   public PLAYER_STATES player_state = PLAYER_STATES.IDLE;
-    // Start is called before the first frame update
     void Start()
     {
         anim = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Debug.Log(player_state);
-       
+        switch (player_state)
+        {
+            case PLAYER_STATES.IDLE:
+                {
+
+                }
+                break;
+            case PLAYER_STATES.LEFT_HIT:
+                {
+
+                }
+                break;
+            case PLAYER_STATES.RIGHT_HIT:
+                {
+
+                }
+                break;
+            default:
+                {
+                    Debug.LogWarning("Player shouldn't enter here. There's a case that's not been coded.");
+                }
+                break;
+        }
     }
-    public void RightPunch()
+    public void RequestRightPunch()
     {
-        player_state = PLAYER_STATES.RIGHT_HIT;
-        anim.SetTrigger("RightHit");
+        if (player_state == PLAYER_STATES.IDLE)
+        {
+            player_state = PLAYER_STATES.RIGHT_HIT;
+            anim.SetTrigger("RightHit");
+        }
+        else
+        {
+            //TODO: Play error sound / tired
+        }
     }
 
-    public void LeftPunch()
+    public void RequestLeftPunch()
     {
-        player_state = PLAYER_STATES.LEFT_HIT;
-        anim.SetTrigger("LeftHit");
+        if (player_state == PLAYER_STATES.IDLE)
+        {
+            player_state = PLAYER_STATES.LEFT_HIT;
+            anim.SetTrigger("LeftHit");
+        }
+        else
+        {
+            //TODO: Play error sound / tired
+        }
+
+    }
+
+    public void RequestRightDodge()
+    {
+        if (player_state == PLAYER_STATES.IDLE)
+        {
+        }
+        else
+        {
+            //TODO: Play error sound / tired
+        }
+    }
+
+    public void RequestLeftDodge()
+    {
+        if (player_state == PLAYER_STATES.IDLE)
+        {
+        }
+        else
+        {
+            //TODO: Play error sound / tired
+        }
     }
 }
