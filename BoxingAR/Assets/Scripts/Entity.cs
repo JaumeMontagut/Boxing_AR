64959fc3e66a266dbf25ce17bbd54ad14e23d0f4
JaumeMontagut <jaume.montagut.i.guix@gmail.com>
2020-05-16 18:09:47 +0200

    adding common code between player and enemy

--- BoxingAR/Assets/Scripts/Entity.cs
+++ BoxingAR/Assets/Scripts/Entity.cs
@@ -16,7 +16,7 @@ public class Entity : MonoBehaviour
         DODGE_RECOVERY,
     };
 
-    public enum PLAYER_POSITION
+    public enum DIRECTION
     {
         LEFT,
         MIDDLE,
@@ -24,8 +24,43 @@ public class Entity : MonoBehaviour
     };
 
     [HideInInspector] public PLAYER_STATES player_state = PLAYER_STATES.IDLE;
-    [HideInInspector] public PLAYER_POSITION player_pos = PLAYER_POSITION.MIDDLE;
+    [HideInInspector] public DIRECTION player_pos = DIRECTION.MIDDLE;
     [HideInInspector] public float life;
+    
+    private GameSystem gameManager;
+
+    public void Start()
+    {
+        gameManager = FindObjectOfType<GameSystem>();
+    }
+
+    public void CheckHit(DIRECTION punchDir, float damage)
+    {
+        if (punchDir == DIRECTION.LEFT)
+        {
+            if (player_pos == DIRECTION.LEFT || player_pos == DIRECTION.MIDDLE)
+            {
+                life -= damage;
+                if (IsDead())
+                {
+                    gameManager.GameOver();
+                }
+            }
+            //TODO: Possible thing, add "tired" frames where you are exposed if you miss
+        }
+        else if (punchDir == DIRECTION.RIGHT)
+        {
+            if (player_pos == DIRECTION.RIGHT || player_pos == DIRECTION.MIDDLE)
+            {
+                life -= damage;
+                if (IsDead())
+                {
+                    gameManager.GameOver();
+                }
+            }
+            //TODO: Possible thing, add "tired" frames where you are exposed if you miss
+        }
+    }
 
     public bool IsDead()
     {
---
+++ BoxingAR/Assets/Scripts/GameSystem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSystem : MonoBehaviour
+{
+    //TODO: Make this a singleton
+
+    public void GameOver()
+    {
+        //TODO: Show Game Over UI, etc.
+    }
+}
